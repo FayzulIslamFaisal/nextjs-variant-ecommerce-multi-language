295a2736f77e4e6594ec34719c969470ffa1bc0d
Md Faisal <mfbfaisal@gmail.com>
2025-10-11 22:18:51 +0600

    orginal image added in database

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["loremflickr.com"],
+    domains: ["loremflickr.com", "c8.alamy.com", "namtech.com.au", "webiators.com", "jumpseller.com", "ditindia.com", "medrectech.com", "encrypted-tbn0.gstatic.com", "media.geeksforgeeks.org", "www.businessofapps.com", "d1hdtc0tbqeghx.cloudfront.net"],
   },
 };
 
